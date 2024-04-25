3f3183ea5180b0a024e018b4acb11acd16c6c311
ammarziddan <127589665+ammarziddan@users.noreply.github.com>
2024-04-25 18:48:50 +0700

    Update index.php

---
+++ fathaya/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// buatlah class dan object untuk menampilkan hasil perhitungan luas Persegi Panjang dan segitiga
+
+// variabel1 = panjang atau alas
+// variabel2 = lebar atau tinggi
+
+class Luas {
+    public  $variabel1,
+            $variabel2;
+
+    // buat method constructor
+
+    // buat method luasPersegiPnj()
+
+    // buat method luasSegitiga()
+}
+
+$obj1 = new Luas();
+
+echo $obj->luasPersegiPnj(); // menampilkan Luas P. Panjang
+echo $obj->luasSegitiga(); // menampilkan Luas Segitiga
