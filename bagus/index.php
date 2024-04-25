f12f08f62d7d9ba1d0a5f7f8bf063c6093bd78cf
ammarziddan <127589665+ammarziddan@users.noreply.github.com>
2024-04-25 18:41:04 +0700

    Update index.php

---
+++ bagus/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// buatlah class dan object untuk menampilkan list buku
+// Judul Buku | Penulis, Penerbit (Rp. harga)
+class Buku {
+    // tuliskan sintaks disini
+    // gunakan method constructor
+    // sertakan method getLabel() untuk mencetak label
+}
+
+//buat minimal 2 objek
+$obj1 = new Buku();
+$obj2 = new Buku();
+
+// panggil function getLabel() dibawah sini untuk semua object
