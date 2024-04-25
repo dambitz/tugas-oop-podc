c3a7721d6086e47f38f84fbac7b10aecc61b62f1
ammarziddan <127589665+ammarziddan@users.noreply.github.com>
2024-04-25 18:37:29 +0700

    Update index.php

--- adam/index.php
+++ adam/index.php
@@ -1,12 +1,15 @@
 <?php 
 
-// buatlah class dan object untuk ...
+// buatlah class dan object untuk menampilkan deskripsi film
+// Judul | Rating, Director
 class film {
-    public $judul;
-
-    public function __construct() {
-        
-    }
+    // tuliskan sintaks disini
+    // gunakan method constructor
+    // sertakan method getLabel() untuk mencetak label
 }
 
+//buat minimal 2 objek
 $obj1 = new film();
+$obj2 = new film();
+
+// panggil function getLabel() dibawah sini untuk semua object
