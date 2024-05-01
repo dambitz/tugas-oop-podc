bbae73b24f140f8a45c859d04723f15da6dd8b30
fatayaarp <165269922+fatayaarp@users.noreply.github.com>
2024-05-01 19:28:13 +0700

    Melengkapi index.php
    Membuat method, menambahkan nilai pada object, membuat echo

--- fathaya/index.php
+++ fathaya/index.php
@@ -9,14 +9,25 @@ class Luas {
             $variabel2;
 
     // buat method constructor
-
+    public function __construct($variabel1 = 0, $variabel2 = 0){
+        $this->variabel1 = $variabel1;
+        $this->variabel2 = $variabel2;
+    }
     // method luasPersegiPnj()
-
+    public function luasPersegiPnj(){
+        $luasPersegiPnj = $this->variabel1 * $this->variabel2;
+        return $luasPersegiPnj;
+    }
     // method luasSegitiga()
+    public function luasSegitiga(){
+        $luasSegitiga = 0.5 * ($this->variabel1 * $this->variabel2);
+        return $luasSegitiga;
+    }
 }
 
-$persegiPanjang = new Luas();
-$segitiga = new Luas();
+$persegiPanjang = new Luas(10, 6);
+$segitiga = new Luas(20, 6);
 
-echo $obj->luasPersegiPnj(); // menampilkan Luas P. Panjang
-echo $obj->luasSegitiga(); // menampilkan Luas Segitiga
+echo "Luas Persegi Panjang = ". $persegiPanjang->luasPersegiPnj(). " m2"; // menampilkan Luas P. Panjang
+echo "<hr>";
+echo "Luas Segitiga = ". $segitiga->luasSegitiga(). " m2"; // menampilkan Luas Segitiga
