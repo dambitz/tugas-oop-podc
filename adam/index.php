b6e4a43b3787dc4cb96385de665236f1398fbc34
dambitz <165284423+dambitz@users.noreply.github.com>
2024-05-24 18:00:59 +0700

    Update index.php
    udah kang

--- adam/index.php
+++ adam/index.php
@@ -1,15 +1,40 @@
 <?php 
 
-// buatlah class dan object untuk menampilkan deskripsi film
-// Judul | Rating, Director
+
 class film {
-    // tuliskan sintaks disini
-    // gunakan method constructor
-    // sertakan method getLabel() untuk mencetak label
+    public  $judul = "podc", 
+            $genre = "sci fi", 
+            $rating = 0, 
+            $director = 0;
+    
+    public function __construct($judul = "judul", $genre = "genre", $rating = 0, $director = 0) {
+        $this->judul = $judul;
+        $this->genre = $genre;
+        $this->rating = $rating;
+        $this->director = $director;
+    }
+    
+    public function getLabel() {
+        $str = "Judul : {$this->judul}, Genre : {$this->genre}, Rating : {$this->rating}, Director : {$this->director}";
+        return $str;
+    }
 }
 
-//buat minimal 2 objek
-$obj1 = new film();
-$obj2 = new film();
+$produk1 = new film();
 
-// panggil function getLabel() dibawah sini untuk semua object
+$produk1->judul = "podc";
+$produk1->genre = "sci fi";
+$produk1->rating = 8.0;
+$produk1->director = "adam";
+
+$produk2 = new film();
+
+$produk2->judul = "star wars";
+$produk2->genre = "adventure";
+$produk2->rating = 9.5;
+$produk2->director = "george lucas";
+
+echo $produk1->getLabel();
+echo "<hr>";
+echo $produk2->getLabel();
+?>
